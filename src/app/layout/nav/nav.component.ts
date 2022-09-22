8f57176f6d773dbb2b946c811656f1d609353489
anatanczak <anatkachen@gmail.com>
2022-09-22 19:15:40 +0200

    add cart

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  public itemsNumber = 0;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
-
 }
